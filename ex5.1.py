aa20c60ec73f4cd3b54ed1cb50323d7d0ba9e13b
Thitiporn3131 <92080688+Thitiporn3131@users.noreply.github.com>
2021-10-18 23:03:24 +0700

    Add files via upload

---
+++ ex5.1.py
@@ -0,0 +1,11 @@
+x = int(input("first number:"))
+y = int(input("second number:"))
+sum = x + y
+different = x - y
+multiple = x * y
+divide = int(x / y)
+
+print(x," + ",y," = ",sum)
+print(x," - ",y," = ",different)
+print(x," x ",y," = ",multiple)
+print(x," / ",y," = ",divide)
